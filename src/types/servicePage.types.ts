cf113c09cc5720d05722f9be40903ea43fdba53d
Paweł <pawelzydek29@gmail.com>
2026-01-22 18:05:37 +0100

    fix: resolve interface naming and upgrade linter

--- src/types/servicePage.types.ts
+++ src/types/servicePage.types.ts
@@ -6,7 +6,7 @@ import { Pricing_Section } from '@/sections/Pricing_Section/Pricing_Section';
 import { OtherOffer } from '@/sections/OtherOffer/OtherOffer';
 import { FAQ_Section } from '@/sections/FAQ_Section/FAQ_Section';
 import { Review_Section } from '@/sections/Review_Sectoin/Review_Section';
-import { ProfitsCTAPProps } from '@/components/ProfitsCTA/ProfitsCTA';
+import { ProfitsCTAProps } from '@/components/ProfitsCTA/ProfitsCTA';
 import { MetadataBaseParams } from '@/utils/createMetadata';
 import { ServicePage_Content } from '@/sections/ServicePage_Content/ServicePage_Content';
 import { SubscriptionCTA } from '@/components/SubscriptionCTA/SubscriptionCTA';
@@ -25,5 +25,5 @@ export interface ServiceContentConfig {
 	otherOffer: ComponentProps<typeof OtherOffer>;
 	faqSection: ComponentProps<typeof FAQ_Section>;
 	reviewSection: ComponentProps<typeof Review_Section>;
-	footerCTA: ProfitsCTAPProps;
+	footerCTA: ProfitsCTAProps;
 }
